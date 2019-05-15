240ce278d2c5ffaaadd69403f4cfebb0d813b8a8
DwightCarterJr <carter91jr@gmail.com>
2019-05-15 14:45:18 -0400

    changes

---
+++ src/components/navbar.vue
@@ -0,0 +1,26 @@
+<template>
+<el-container>
+  <el-aside>
+<el-row>
+  <el-col :xs="8" :sm="6" :md="15" :lg="20" :xl="20"><div class="grid-content bg-red">
+        <h1>Dwight Carter</h1>
+  </div>
+  </el-col>
+</el-row>    
+</el-aside>
+    </el-container>
+</template>
+
+<style>
+
+  .bg-red {
+    background: #ef220e;
+    height:100vh;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    min-height: 36px;
+  }
+</style>
